c517190d49c08b47ddbe69a55ad151ee89249fa5
Neil Clarke <neil.clarke@payd.co>
2017-05-13 15:44:39 +0200

    file parsing WIP

--- dev/user.clj
+++ dev/user.clj
@@ -1,10 +1,2 @@
-(ns user
-  [:use [clojure.java.io]])
-
-(let [filename "resources/input.txt"
-      parse-grid (fn [grid]
-                    (->> (clojure.string/split grid #" ")
-                      (map #(Integer/parseInt %))
-                      (into [])))]
-  (with-open [rdr (reader filename)]
-    (parse-grid (.readLine rdr))))
+(ns user)
+  
