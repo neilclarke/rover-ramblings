3384a01de6319345cb12ee41d4dba9642836ff02
Neil Clarke <neil.clarke@payd.co>
2017-05-13 17:12:06 +0200

    processing WIP

--- src/clj/rover/core.clj
+++ src/clj/rover/core.clj
@@ -1,4 +1,47 @@
-(ns rover.core)
+(ns rover.core
+  [:require [clojure.java.io :as io]
+            [rover.drive :as drive]])
+
+(->> "RFRFRFRF"
+  (seq)
+  (into [])
+  (map #(String/valueOf %))
+  (map keyword))
+
+(let [rover-start-seq (clojure.string/split "1 1 E" #" ")
+      rover-start (->> rover-start-seq
+                      (butlast)
+                      (map #(Integer/parseInt %))
+                      (into []))
+      rover-orient (->> rover-start-seq
+                      (last)
+                      (keyword))]
+    [rover-start rover-orient])
+
+(let [filename "resources/input.txt"
+      parse-grid (fn [grid]
+                    (->> (clojure.string/split grid #" ")
+                      (map #(Integer/parseInt %))
+                      (into [])))]
+  (with-open [reader (io/reader filename)]
+    (let [grid-size (parse-grid (.readLine reader))
+          rover-setups (->> (line-seq reader)
+                          (reduce conj [])
+                          (remove empty?)
+                          (partition 2))]
+        rover-setups)))
+
+(let [grid-size [10 10]
+      rover (atom [[2 2] :W])
+      commands [:R :F :F :F :R :F :F :R :F :L :F :L :F :F]
+      lost-zones (atom #{})]
+    (for [command commands]
+      (let [last-known-pos (first @rover)
+            new-state (drive/drive last-known-pos (second @rover) command @lost-zones)]
+        (if (= new-state :LOST)
+            (swap! lost-zones conj (first @rover))
+            (reset! rover new-state)))))
+
 
 (defn -main
   "Main"
--- src/clj/rover/drive.clj
+++ src/clj/rover/drive.clj
@@ -1,6 +1,8 @@
 (ns rover.drive)
 
 (defn rotate
+  "Returns the new orientation for the rover based on the rotation applied to the
+  current orientation"
   [heading direction]
   (let [directions [:N :E :S :W]
         dir-count (count directions)
@@ -17,6 +19,7 @@
       (nth directions new-heading)))
 
 (defn advance
+  "Advances the rover in the grid, based on its current orientation"
   [heading position]
   (let [[x y] position]
     (case heading
@@ -25,3 +28,16 @@
       :S [x (dec y)]
       :W [(dec x) y]
       position)))
+
+(defn drive
+  "Advance or rotate, ignoring commands to enter lost zones"
+  [position heading command lost-zones]
+  (cond
+
+    (= command :F)
+    (let [new-pos (advance heading position)]
+      (if (contains? lost-zones new-pos) [position heading] [new-pos heading]))
+
+    (or (= :L command) (= :R command)) [position (rotate heading command)]
+
+    :else [position heading]))
